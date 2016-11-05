6e64a62ed50615d028144cb8e6438fe33a4c843f
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-11-05 15:37:24 +0900

    Add notification messages

--- build_vim.sh
+++ build_vim.sh
@@ -3,10 +3,12 @@ set -eu
 PREFIX=$HOME/local
 WORKDIR=$PREFIX/src/vim
 
-COMMANDS="cd $WORKDIR
+COMMANDS="echo 'building vim...'
+cd $WORKDIR
 git pull
 ./configure --prefix=$PREFIX --with-features=huge --enable-gui=gnome2 --enable-fail-if-missing --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp
 make -j8
-make install"
+make install
+echo 'done.'"
 
 echo "$COMMANDS"
