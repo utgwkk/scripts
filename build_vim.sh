b3169cae637970552d2efb2e717780ed6b0747e8
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-11-05 15:35:58 +0900

    dry-run mode
    Install with 'sh build_vim.sh | sh'

--- build_vim.sh
+++ build_vim.sh
@@ -3,8 +3,10 @@ set -eu
 PREFIX=$HOME/local
 WORKDIR=$PREFIX/src/vim
 
-cd $WORKDIR
+COMMANDS="cd $WORKDIR
 git pull
 ./configure --prefix=$PREFIX --with-features=huge --enable-gui=gnome2 --enable-fail-if-missing --enable-perlinterp --enable-pythoninterp --enable-python3interp --enable-rubyinterp
 make -j8
-make install
+make install"
+
+echo "$COMMANDS"
